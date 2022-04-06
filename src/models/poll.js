a2483ae02ac4a38c79bad38e2a6b37d0861d235a
Caio Teixeira <tex.caio@gmail.com>
2022-04-05 21:43:11 -0300

    fix schema

--- src/models/poll.js
+++ src/models/poll.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
 
+const ChoiceSchema = mongoose.Schema({
+  id: String,
+  value: String,
+  numberOfVotes: Number
+})
+
 const PollSchema = mongoose.Schema({
   id: String,
   topic: String,
-  choices: [{
-    id: String,
-    value: String,
-    numberOfVotes: Number
-  }]
+  choices: [ChoiceSchema],
+  isActive: Boolean
 })
 
 module.exports = mongoose.model('Poll', PollSchema)
